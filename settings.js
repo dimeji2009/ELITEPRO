91ca45ab6186779d0a49b4ba587540a1d653a0bc
dimeji2009 <witnessramadan@gmail.com>
2025-01-11 13:46:55 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,15 +2,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348109263390"
-global.ownername = "ElitePro"
+global.ownernomer = "2349155434840"
+global.ownername = "Dimeji"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
 global.location = "Nigeria, Port Harcourt"
 
-global.ownernumber = '2348109263390'  //creator number
-global.ownername = 'ElitePro' //owner name
-global.botname = 'ELITE-PRO' //name of the bot
+global.ownernumber = '2349155434840'  //creator number
+global.ownername = 'Dimeji' //owner name
+global.botname = 'Dimeji' //name of the bot
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
